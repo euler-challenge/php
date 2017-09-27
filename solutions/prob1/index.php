3a20c1f20d861d3b405a189a49c8a7ad950451b5
Sparkling Comet <shanerx@shanerx.org>
2017-09-27 20:21:19 +0200

    Fix misunderstanding in #1

--- solutions/prob1/index.php
+++ solutions/prob1/index.php
@@ -1,14 +1,13 @@
 <?php
 
-$n = 0;
+$n = 1;
+$sum = 0;
 
 while ($n <= 1000)
 {
     if ($n % 3 == 0 || $n % 5 == 0) {
-        if ($n == 1000)
-            print $n;
-        else
-            print "$n, ";
+        $sum += $n;
     }
     $n++;
 }
+print $sum;
